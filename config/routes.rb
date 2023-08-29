86fc59e0d1c50b0346e7e4830a5ff1f9b95e683b
samy bougdim <samy.bougdim@live.fr>
2023-08-29 10:57:01 +0200

    routes clear

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'user_categories/index'
-  get 'user_categories/show'
-  get 'user_categories/new'
-  get 'user_categories/create'
-  get 'user_categories/edit'
-  get 'user_categories/update'
-  get 'user_categories/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
