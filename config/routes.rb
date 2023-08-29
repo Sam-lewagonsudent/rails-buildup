1a561e2f5f9622bdd7135f1fe93187a64c1c0342
samy bougdim <samy.bougdim@live.fr>
2023-08-29 16:09:58 +0200

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get '/redirection', to: 'pages#redirection', as: 'redirection'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   resources :actions, only: [:index, :show] do
-    resources :user_challenges
+    resources :user_challenges, only: [:create]
   end
+  resources :user_challenges, only: [:index, :show, :update]
   resources :user_categories
   resources :categories, only: [:index, :show]
 end
