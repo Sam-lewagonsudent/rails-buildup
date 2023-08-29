9dee8b1d40748c254f1adc576bae936f508e53c8
samy bougdim <samy.bougdim@live.fr>
2023-08-29 17:29:46 +0200

    rien de spe

--- app/controllers/user_categories_controller.rb
+++ app/controllers/user_categories_controller.rb
@@ -1,6 +1,6 @@
 class UserCategoriesController < ApplicationController
   def index
-    UserCategory.all
+    @user_categories = UserCategory.where(user_id: current_user.id)
   end
 
   def show
@@ -10,6 +10,11 @@ class UserCategoriesController < ApplicationController
   end
 
   def create
+  @user_category = UserCategory.new(user_category_params)
+  @user_category.user = current_user
+  @user_category.category = Category.find(params[:category_id])
+  @user_category.save
+  redirect_to root_path
   end
 
   def edit
@@ -20,4 +25,12 @@ class UserCategoriesController < ApplicationController
 
   def destroy
   end
+
+
+  private
+
+  def user_category_params
+    params.require(:user_category).permit(:user_id, :category_id)
+  end
+
 end
