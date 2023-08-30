5db69d6de3ab8f0a72721de814ae5f2086c0f807
samy bougdim <samy.bougdim@live.fr>
2023-08-30 10:36:36 +0200

    user_categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
 
   def index
+    @user = current_user
     @categories = Category.all
+    @user_categories = UserCategory.new
+    @category = Category.new
   end
 
   def show
