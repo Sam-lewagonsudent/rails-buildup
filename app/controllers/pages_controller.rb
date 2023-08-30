3f1d31fbd7df1e0d9394c3ec9c0b45204bcfd913
samy bougdim <samy.bougdim@live.fr>
2023-08-30 15:29:35 +0200

    rien

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @total_value = current_user.total_value_of_completed_actions
+    if user_signed_in?
+      @total_value = current_user.total_value_of_completed_actions
+    end
   end
 end
