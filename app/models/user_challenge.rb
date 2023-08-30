98b313ca062093efae215ce89b3c9f6c3b3a0f70
anthony lambert <lamberta.mailbox@gmail.com>
2023-08-30 15:27:00 +0200

    test

--- app/models/user_challenge.rb
+++ app/models/user_challenge.rb
@@ -13,6 +13,8 @@ class UserChallenge < ApplicationRecord
   def action_once_per_day
     if UserChallenge.where(user_id: user_id, action_id: action_id).where("DATE(created_at) = ?", Date.today).exists?
       errors.add(:base, "You've already added this challenge today.")
+    elsif UserChallenge.where(user_id: user_id, action_id: action_id, done: false).exists?
+      errors.add(:base, "You've already added this challenge.")
     end
   end
 end
