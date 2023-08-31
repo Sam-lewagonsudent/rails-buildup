8ce7934d3c4a4c52918eb3fbf72351bce54aa3f9
anthony lambert <lamberta.mailbox@gmail.com>
2023-08-31 14:18:40 +0200

    pour jo par jo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-     if user_signed_in?
-        @total_value = current_user.total_value_of_completed_actions
-        @ranked_users = User.ranked_by_total_value
-        @ranked_categories = Category.ranked_by_total_value
-        @categories = Category.all
-      end
+    if user_signed_in?
+      @total_value = current_user.total_value_of_completed_actions
+      @ranked_users = User.ranked_by_total_value
+      @ranked_categories = Category.ranked_by_total_value
+      @categories = Category.all
+    end
   end
-
 
   def profile
     @completed_chal_historic = UserChallenge.where(user_id: current_user.id, done: true)
   end
-
 end
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
-  }
+//   search(event) {
+//     event.preventDefault(); // Prevent default form submission.
+
+//       let url = new URL(this.element.action);
+//       let params = new URLSearchParams(new FormData(this.element));
+//       url.search = params.toString();
+// console.log('test');
+//       fetch(url, {
+//           method: this.element.method,
+//           headers: {
+//             'X-Requested-With': 'XMLHttpRequest',
+//             'Accept': 'text/javascript'
+//           },
+//       })
+//       .then(response => response.text())
+//       .then(data => {
+//           // Handle the returned data
+//       })
+//       .catch(error => console.error('Error:', error));
+//   }
+  // search() {
+  //   this.element.requestSubmit()
+  //   console.log('test');
+  // }
 }
