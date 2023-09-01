d093100fceced06f6949dc7bcce189ebb19a70ce
anthony lambert <lamberta.mailbox@gmail.com>
2023-09-01 09:18:41 +0200

    fix fail

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
       @categories = Category.all
       @user = current_user
       @user_level = calculate_level(@total_value)
-       @user_challenges_not_completed = UserChallenge.where(user_id: current_user.id, done: false)
+      @user_challenges_not_completed = UserChallenge.where(user_id: current_user.id, done: false)
     end
   end
 
@@ -21,12 +21,12 @@ class PagesController < ApplicationController
     @user_categories = UserCategory.where(user_id: current_user.id)
   end
 
-
-  def  impact
+  def impact
   end
 
   def convertisseur
   end
+
   def calculate_total_value(user)
     completed_actions = user.user_challenges.where(done: true)
     total_value = completed_actions.sum(:value)
@@ -42,6 +42,5 @@ class PagesController < ApplicationController
     user_level = (total_value / 100) + 1
     remaining_progress = total_value % 100
     [user_level, remaining_progress]
-
-
+  end
 end
